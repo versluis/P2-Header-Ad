0222489477b01afef2401b771669662355d6d28d
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2013-10-18 23:08:00 -0400

    header ad works great - but can't post with P2 anymore :-(

--- p2-header-ad-script.js
+++ p2-header-ad-script.js
@@ -4,3 +4,12 @@
 // append our #p2HeaderAd to the current #header
 var $p2HeaderAdContent = jQuery('#p2HeaderAd');
 jQuery('#header').append($p2HeaderAdContent);
+
+// 
+jQuery('#p2HeaderAd').css('visibility', 'visible');
+
+// we could make our ad visible via fadeIn - see http://api.jquery.com/fadeIn/
+// lovely approach, but doesn't play nice with P2 :-(
+// jQuery('#p2HeaderAd').delay(1000).fadeIn('slow', function () {
+	// could add something here upon completion	
+// });
